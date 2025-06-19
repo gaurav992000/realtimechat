9613b02db92243716f08fc947c7934aba0860572
gaurav992000 <144047956+gaurav992000@users.noreply.github.com>
2025-06-19 21:02:14 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ import { Server } from "socket.io"
 
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5173"
+        origin:"https://realtimechat-ylxp.onrender.com"
     }
 })
 export const getReceiversocketid=(receiver)=>{
